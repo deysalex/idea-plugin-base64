137942e9efa308f84d18a6111d5f44bcb94f8508
Deys <deysalex@gmail.com>
2018-11-26 23:22:23 +0700

    #1 Теряется первоначальный текст в окне base64

--- src/ru/deysa/plugin/base64/base64ToolWindowFactory/Base64ToolWindowFactory.java
+++ src/ru/deysa/plugin/base64/base64ToolWindowFactory/Base64ToolWindowFactory.java
@@ -12,17 +12,8 @@ import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
-/**
- * Created by IntelliJ IDEA.
- * User: Alexey.Chursin
- * Date: Aug 25, 2010
- * Time: 2:09:00 PM
- */
 public class Base64ToolWindowFactory implements ToolWindowFactory {
 
     private JPanel myToolWindowContent;
@@ -30,42 +21,45 @@ public class Base64ToolWindowFactory implements ToolWindowFactory {
     private JTextArea decodeText;
     private ToolWindow myToolWindow;
 
-    private final ScheduledExecutorService scheduler =
-            Executors.newScheduledThreadPool(1);
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
     public Base64ToolWindowFactory() {
 
         encodeText.addFocusListener(new FocusListener() {
-            private ScheduledFuture<?> decodeHandle;
+            private ScheduledFuture<?> future;
+            private ComparableRunnable<String> decoder;
 
             @Override
             public void focusGained(FocusEvent e) {
-                decodeHandle = decodeText(encodeText, decodeText);
+                decoder = decodeText(encodeText, decodeText);
+                future = scheduler.scheduleAtFixedRate(decoder, 1000, 500, TimeUnit.MILLISECONDS);
             }
 
             @Override
             public void focusLost(FocusEvent e) {
-                decodeHandle.cancel(true);
+                CompletableFuture.runAsync(decoder).thenAccept(v -> future.cancel(true));
             }
         });
 
         decodeText.addFocusListener(new FocusListener() {
-            private ScheduledFuture<?> encodeHandle;
+            private ScheduledFuture<?> future;
+            private ComparableRunnable<String> encoder;
 
             @Override
             public void focusGained(FocusEvent e) {
-                encodeHandle = encodeText(encodeText, decodeText);
+                encoder = encodeText(encodeText, decodeText);
+                future = scheduler.scheduleAtFixedRate(encoder, 1000, 500, TimeUnit.MILLISECONDS);
             }
 
             @Override
             public void focusLost(FocusEvent e) {
-                encodeHandle.cancel(true);
+                CompletableFuture.runAsync(encoder).thenAccept(v -> future.cancel(true));
             }
         });
     }
 
-    private ScheduledFuture<?> decodeText(JTextArea from, JTextArea to) {
-        final ComparableRunnable<String> decoder = new ComparableRunnable<String>() {
+    private ComparableRunnable<String> decodeText(JTextArea from, JTextArea to) {
+        return new ComparableRunnable<String>() {
             @Override
             public void run(String value) {
                 to.setText(decode(value));
@@ -76,11 +70,10 @@ public class Base64ToolWindowFactory implements ToolWindowFactory {
                 return from.getText();
             }
         };
-        return scheduler.scheduleAtFixedRate(decoder, 1, 1, TimeUnit.SECONDS);
     }
 
-    private ScheduledFuture<?> encodeText(JTextArea from, JTextArea to) {
-        final ComparableRunnable<String> encoder = new ComparableRunnable<String>() {
+    private ComparableRunnable<String> encodeText(JTextArea from, JTextArea to) {
+        return new ComparableRunnable<String>() {
             @Override
             public void run(String value) {
                 from.setText(encode(value));
@@ -91,7 +84,6 @@ public class Base64ToolWindowFactory implements ToolWindowFactory {
                 return to.getText();
             }
         };
-        return scheduler.scheduleAtFixedRate(encoder, 1, 1, TimeUnit.SECONDS);
     }
 
     public void createToolWindowContent(Project project, ToolWindow toolWindow) {
